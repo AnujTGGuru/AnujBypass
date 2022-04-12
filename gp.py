d603cdb7b2653e7ba16fa8d790512afde2c06c9c
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-04-12 12:50:34 +0530

    Update gp.py

--- gp.py
+++ gp.py
@@ -15,29 +15,35 @@ print("Checking Link...")
 print("Checking Done!")
 # ==============================================
 print("Bypassing the Link...")
-def gplinks_bypass(url):
-    scraper = cloudscraper.create_scraper(allow_brotli=False)
-    res = scraper.get(url)
-    
-    h = { "referer": res.url }
-    res = scraper.get(url, headers=h)
-    
-    bs4 = BeautifulSoup(res.content, 'lxml')
+def gplinks_bypass(url: str):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    p = urlparse(url)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
+
+    res = client.head(url)
+    header_loc = res.headers['location']
+    param = header_loc.split('postid=')[-1]
+    req_url = f'{p.scheme}://{p.netloc}/{param}'
+
+    p = urlparse(header_loc)
+    ref_url = f'{p.scheme}://{p.netloc}/'
+
+    h = { 'referer': ref_url }
+    res = client.get(req_url, headers=h, allow_redirects=False)
+
+    bs4 = BeautifulSoup(res.content, 'html.parser')
     inputs = bs4.find_all('input')
     data = { input.get('name'): input.get('value') for input in inputs }
 
     h = {
-        'content-type': 'application/x-www-form-urlencoded',
-        'x-requested-with': 'XMLHttpRequest'
+        'referer': ref_url,
+        'x-requested-with': 'XMLHttpRequest',
     }
-    
-    time.sleep(10) # !important
-    
-    p = urlparse(url)
-    final_url = f'{p.scheme}://{p.netloc}/links/go'
-    res = scraper.post(final_url, data=data, headers=h).json()
-
-    return res
+    time.sleep(10)
+    res = client.post(final_url, headers=h, data=data)
+    try:
+        return res.json()['url'].replace('\/','/')
+    except: return 'Something went wrong :(
 
 # ==============================================
 
