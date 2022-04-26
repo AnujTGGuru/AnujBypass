0e4da0d5cf282591cd044fe263bef4769eea7785
thanioruvangkls <91356119+thanioruvangkls@users.noreply.github.com>
2022-04-26 12:56:20 +0530

    Update rlb.py

--- rlb.py
+++ rlb.py
@@ -11,28 +11,29 @@ print("Checking Link!")
 
 def rocklinks_bypass(url):
     client = cloudscraper.create_scraper(allow_brotli=False)
-    DOMAIN = "https://link.techyone.co"
+    DOMAIN = "https://rocklink.in"
+    
     url = url[:-1] if url[-1] == '/' else url
 
     code = url.split("/")[-1]
-    final_url = f"{DOMAIN}/{code}?quelle="
+    final_url = f"{DOMAIN}/{code}"
 
     resp = client.get(final_url)
-    
     soup = BeautifulSoup(resp.content, "html.parser")
-    try:
-        inputs = soup.find(id="go-link").find_all(name="input")
-    except:
-        return "Incorrect Link"
+    
+    try: inputs = soup.find(id="go-link").find_all(name="input")
+    except: return "Incorrect Link"
+    
     data = { input.get('name'): input.get('value') for input in inputs }
 
     h = { "x-requested-with": "XMLHttpRequest" }
     
-    time.sleep(6)
+    time.sleep(5)
     r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
     try:
         return r.json()['url']
     except: return "Something went wrong :("
+
 
 # ----------------------------------
 
