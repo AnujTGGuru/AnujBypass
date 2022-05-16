11250c9a885c0000122cd0b66ab374f6f727fbd9
majnurangeela <84721324+majnurangeela@users.noreply.github.com>
2022-05-16 10:08:56 +0530

    Fix

--- gp.py
+++ gp.py
@@ -45,6 +45,6 @@ def gplinks_bypass(url: str):
 # ==============================================
 
 inf = gplinks_bypass(url)
-print(print("❤️✨BYPASSED GPLINKS LINK: "+ inf + " ❤️✨" ,file=open("2.txt", "w"))
+print("❤️✨BYPASSED GPLINKS LINK: "+ inf + " ❤️✨" ,file=open("2.txt", "w"))
 print("Confirming Link...")
 print("Successfully Bypassed!")
