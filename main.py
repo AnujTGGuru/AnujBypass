ed539bd3ce68da1842bd63a088444d27551a4955
Primecloudg <102500355+Primecloudg@users.noreply.github.com>
2022-05-21 18:14:47 +0530

    V

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from urllib.parse import urlparse, parse_qs
 TOKEN = environ.get('BOT_TOKEN')
 
 def start(update, context):
-    update.message.reply_text("Hello Friends! I will bypass the shortlinks and Generate Direct GDrive Link")
+    update.message.reply_text("Hello Friends! I Workes Only Kristy's Mirror Group")
     
 def help(update, context):
    update.message.reply_text("""
