809a10544d2387c82719bbdac38f399beb0f2413
Primecloudg <102500355+Primecloudg@users.noreply.github.com>
2022-05-21 13:45:47 +0530

    Main

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from urllib.parse import urlparse, parse_qs
 TOKEN = environ.get('BOT_TOKEN')
 
 def start(update, context):
-    update.message.reply_text("Hello folks! I will bypass the shortlinks and Generate Direct GDrive Link")
+    update.message.reply_text("Hello Friends! I will bypass the shortlinks and Generate Direct GDrive Link")
     
 def help(update, context):
    update.message.reply_text("""
